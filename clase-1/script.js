fd937ef9eb1bc485910eb1762b3eb7ddc6dcf86e
Francisco Pugh <franciscopugh01@gmail.com>
2022-06-16 14:37:16 -0300

    Clase 1

---
+++ clase-1/script.js
@@ -0,0 +1,42 @@
+// Comentario de una linea
+/*
+    Comentario multi-linea
+
+*/
+/**
+ * Comentario multi-linea
+ */
+/*
+var nombre = 'Francisco' //String o texto
+var apellido = "Pugh"
+var sueldo = 1050 //Number o numero
+var esProfesor = true //Boolean o logicas es_profesor
+var esPropietario = null //null o vacio
+var esPadre = undefined //undefined o indefinido
+
+//alert(esPadre)
+//console.log(esPadre)
+
+console.log(nombre)
+nombre = 1421456232
+console.log(nombre)
+nombre = true
+console.log(nombre)
+nombre = null
+console.log(nombre)
+nombre = undefined
+console.log(nombre)
+
+*/
+var apellido = "Pugh"
+apellido = "Rodriguez" //Borrar apellido y crear var apellido con otro valor
+
+let nombre = "Francisco"
+console.log(apellido)
+nombre = 12321
+console.log(nombre)
+
+const IVA = 1.21
+
+IVA = 4224
+
